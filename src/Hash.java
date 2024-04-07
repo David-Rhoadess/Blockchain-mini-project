2c7fa3b388a93d532c12a9d6395e723dab9b29ce
DAVID_RHOADES <RHOADESD@grinnell.edu>
2024-04-07 16:18:54 -0500

    Started project, Hash class completed

---
+++ src/Hash.java
@@ -0,0 +1,50 @@
+
+/**
+ * Sort using merge sort.
+ *
+ * @author David Rhoades
+ * @author Zack Abdilah
+ */
+
+public class Hash{
+
+
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+  byte[] byteArray;
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  // constructs a new Hash object that contains the given hash (as an array of bytes)
+  public Hash(byte[] data){
+    this.byteArray = data;
+
+  }// Hash Method
+
+  // +---------+-----------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  //returns the hash contained in this object.
+  public byte[] getData(){
+    return this.byteArray;
+  }
+
+  //returns true if this hash meets the criteria for validity, i.e., its first three indices contain zeroes.
+  public boolean isValid(){
+    return (this.byteArray[0] == 0 && this.byteArray[1] == 0 && this.byteArray[2] == 0);
+  }
+
+  //returns the string representation of the hash as a string of hexadecimal digits, 2 digits per byte.
+  public String toString(){
+    String tmp = "";
+    for (int i = 0; i < this.byteArray.length; i++) {
+      tmp += String.format("%x", Byte.toUnsignedInt(this.byteArray[i]));
+    }
+    return tmp;
+  }
+
+} // class Hash
